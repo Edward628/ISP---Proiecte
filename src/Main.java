aea60286ec72aef3b69ce1ae55ea1e72e00d5614
Eduard <eduardraul.suciu@ulbsibiu.ro>
2026-03-16 14:33:52 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
 
 void main() {
- Student st=new Student(123,"Gabriel","Tamasel","ISM_21/2");
-    System.out.println(st);
+     List<Student> studenti = new ArrayList<>();
+     studenti.add(new Student(101, "Ion", "Popescu", "TI21/1"));
+     studenti.add(new Student(102, "Ana", "Ionescu", "TI21/2"));
+     studenti.add(new Student(120, "Alis", "Popa", "TI21/2"));
+    System.out.println("Lista de studenti:");
+    for (Student s : studenti) {
+        System.out.println(s);
+    }
+
+    Student student1 = new Student(120, "Alis", "Popa", "TI21/2");
+    System.out.println("\nStudentul " + student1 + " este in lista? " + Student.existaStudent(studenti, student1));
+
+    Student student2 = new Student(112, "Maria", "Popa", "TI21/1");
+    System.out.println("Studentul " + student2 + " este in lista? " + Student.existaStudent(studenti, student2));
+
+
 }
--- src/Student.java
+++ src/Student.java
@@ -1,18 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
     int numarMatricol;
     String prenume;
     String nume;
     String formatieDeStudiu;
 
-    Student(int x,String s,String t,String m){
-        numarMatricol=x;
-        prenume=s;
-        nume=t;
-        formatieDeStudiu=m;
+    Student(int x, String s, String t, String m) {
+        numarMatricol = x;
+        prenume = s;
+        nume = t;
+        formatieDeStudiu = m;
     }
 
-public String toString(){
-        return("numarmatricol:"+numarMatricol+" prenume:"+prenume+" nume:"+nume+" formatieDeStudiu:"+formatieDeStudiu);
-}
+    public String toString() {
+        return "numarmatricol:" + numarMatricol + " prenume:" + prenume + " nume:" + nume + " formatieDeStudiu:" + formatieDeStudiu;
+    }
+    public static boolean existaStudent(List<Student> lista, Student s) {
+        for (Student st : lista) {
+            if (st.prenume.equals(s.prenume) &&
+                    st.nume.equals(s.nume) &&
+                    st.formatieDeStudiu.equals(s.formatieDeStudiu)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
