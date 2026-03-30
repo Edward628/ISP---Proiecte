7770dea802a0174c1ddef48eed3b29e31aa4c98e
Eduard <eduardraul.suciu@ulbsibiu.ro>
2026-03-30 15:36:30 +0300

    Initial commit

--- src/Student.java
+++ src/Student.java
@@ -2,12 +2,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Objects;
 
 public class Student {
     int numarMatricol;
     String prenume;
     String nume;
     String formatieDeStudiu;
+    double nota;
 
     Student(int x, String s, String t, String m) {
         numarMatricol = x;
@@ -15,9 +17,29 @@ public class Student {
         nume = t;
         formatieDeStudiu = m;
     }
+    public void setNota(double nota)
+    { this.nota = nota;
+
+    }
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return numarMatricol == student.numarMatricol;
+    }
+
+
+    public int hashCode() {
+        return Objects.hash(numarMatricol);
+    }
+
 
     public String toString() {
-        return "numarmatricol:" + numarMatricol + " prenume:" + prenume + " nume:" + nume + " formatieDeStudiu:" + formatieDeStudiu;
+        return "NrMatricol: " + numarMatricol +
+                " | Prenume: " + prenume +
+                " | Nume: " + nume +
+                " | Grupa: " + formatieDeStudiu +
+                " | Nota: " + nota;
     }
     public static boolean existaStudent(List<Student> lista, Student s) {
         for (Student st : lista) {
